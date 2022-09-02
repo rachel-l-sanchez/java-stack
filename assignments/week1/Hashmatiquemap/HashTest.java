a4b8dfdffd654e9de4d3be984098a5f3348200eb
Rachel Sanchez <rachel.sanchez@live.com>
2022-09-02 14:21:45 -0700

    hashMap assignment

---
+++ assignments/week1/Hashmatiquemap/HashTest.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class HashTest {
+       public static void main(String[] args) {
+              // object instance
+              HashMatique appTesting = new HashMatique();
+              // testing the methods
+              System.out.println("----- getLyric Total Test-----");
+              appTesting.getLyric("Truth Hurts");
+              System.out.println("----- getAll Total Test-----");
+              appTesting.getAll();
+
+       }
+}
