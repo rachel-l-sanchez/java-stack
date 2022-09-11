6079cd3e1475aea1fab5457759e014bbadab48c1
Rachel Sanchez <rachel.sanchez@live.com>
2022-09-11 13:28:05 -0700

    template rendering

---
+++ assignments/week3/hoppersreceipt/src/main/java/com/rachels/HopperController.java
@@ -0,0 +1,38 @@
+package com.rachels;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+@Controller
+public class HopperController {
+	@RequestMapping("/query")
+	public String query(@RequestParam(value="q", required=false) String searchQuery) {
+	    return "You searched for: " + searchQuery;
+	}
+
+	// class definition and imports here...
+    @RequestMapping("/")
+    public String index(Model model) {
+        
+        String name = "Grace Hopper";
+        String itemName = "Copper wire";
+        double price = 5.25;
+        String description = "Metal strips, also an illustration of nanoseconds.";
+        String vendor = "Little Things Corner Store";
+    
+        model.addAttribute("fullname", name);
+        model.addAttribute("item",itemName);
+        model.addAttribute("price", price);
+        model.addAttribute("description", description);
+        model.addAttribute("vendor", vendor);
+        
+        return "index.jsp";
+    }
+   
+
+
+}
---
+++ assignments/week3/hoppersreceipt/src/main/java/com/rachels/HoppersreceiptApplication.java
@@ -0,0 +1,15 @@
+package com.rachels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Controller
+public class HoppersreceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoppersreceiptApplication.class, args);
+	}
+
+}
