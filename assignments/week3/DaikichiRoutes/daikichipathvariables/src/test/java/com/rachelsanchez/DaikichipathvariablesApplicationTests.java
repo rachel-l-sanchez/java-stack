24dfc5b268ee8f6eeff8a1eeb5d7f495f9a41ff4
Rachel Sanchez <rachel.sanchez@live.com>
2022-09-11 11:12:16 -0700

    init

---
+++ assignments/week3/DaikichiRoutes/daikichipathvariables/src/test/java/com/rachelsanchez/DaikichipathvariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rachelsanchez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichipathvariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
